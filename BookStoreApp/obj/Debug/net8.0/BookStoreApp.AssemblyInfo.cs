4d0b729793b8dd25a58e222d8f536662acc8b4ca
bahiraeimmai@gmail.com <bahiraeimmai@gmail.com>
2025-03-14 12:55:50 +0330

    &&

--- BookStoreApp/obj/Debug/net8.0/BookStoreApp.AssemblyInfo.cs
+++ BookStoreApp/obj/Debug/net8.0/BookStoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2a4a345f1c81e2f253ed6a3b2c69b3305e1239f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3531e7873fb52609288de50f1a778271aab70c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
