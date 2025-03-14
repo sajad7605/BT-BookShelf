3e133d34ea5df3d984ec5cc2d5d62247868bf8e8
bahiraeimmai@gmail.com <bahiraeimmai@gmail.com>
2025-03-14 06:26:14 +0330

    migrations commit

--- BookStoreApp/obj/Debug/net8.0/BookStoreApp.AssemblyInfo.cs
+++ BookStoreApp/obj/Debug/net8.0/BookStoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+471ded5a186492c6c08d4c3952bba08c25470be3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd08a2c7d35d58fea39bab8cdbe76e4b7093c2af")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
